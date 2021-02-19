4c8a4ce846f9b7154b4900b2a2a7ecf4abf15d93
Ju Dantas <jdantas@thoughtworks.com>
2021-02-19 19:31:36 -0300

    feat(dices): add functions for dices rolls

---
+++ src/dices/index.js
@@ -0,0 +1,14 @@
+import { randomInt } from './randomInt'
+
+export const d6 = ({ bonus, rolls }) => {
+  if (!bonus) {
+    bonus = 0
+  }
+  if (!rolls) {
+    rolls = 1
+  }
+
+  return rolls * randomInt(1, 6) + bonus
+}
+
+export const d66 = () => parseInt(`${d6({})}${d6({})}`)
