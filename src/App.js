fa08d0eacf95ee54b27e099d9c739512001c16e6
Ju Dantas <jdantas@thoughtworks.com>
2021-02-19 03:13:37 -0300

    chore: added responsive rules for encounter item and form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Encounter from './components/encounters/component'
+import './app.css'
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="app">
+      <p><strong>Encontros aleatórios</strong></p>
       <Encounter />
+
+      <span className="developed-by">by <a href="https://github.com/narukaioh">@narukaioh</a></span>
     </div>
   );
 }
