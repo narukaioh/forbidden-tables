3b330e3123f1d5d803cce4ea22d9f51bced39637
Ju Dantas <jdantas@thoughtworks.com>
2021-03-20 00:47:39 -0300

    feat(dices): turn dices randomly

--- src/pages/dice-page.js
+++ src/pages/dice-page.js
@@ -1,43 +1,76 @@
+import { useState } from 'react'
 import DiceComponent from '../components/dice'
 import PageDefault from './page-default'
+import { d6 } from '../dices'
 
 const DicePage = () => {
+
+  const [state, setState] = useState({ 
+    attribute: 0, 
+    skill: 0, 
+    gear: 0,
+    attributesDices: [],
+    skillDices: [],
+    gearDices: []
+  })
+
+  const handleChange = (event) => {
+    const { value, name } = event.target
+    setState({
+      ...state,
+      [name]: parseInt(value)
+    })
+  }
+
+  const handleClick = (event) => {
+    let attributesDices = [...Array(state.attribute)].map(i => d6({}))
+    let skillDices = [...Array(state.skill)].map(i => d6({}))
+    let gearDices = [...Array(state.gear)].map(i => d6({}))
+
+    setState({
+      ...state,
+      attributesDices,
+      skillDices,
+      gearDices
+    })
+  }
+
   return (
     <PageDefault>
       
       <header className="dices-header">
         <p>
           <strong>Atributo</strong>
-          <input type="number" placeholder="Ex: 2, 3" />
+          <input onChange={handleChange} type="number" name="attribute" placeholder="Ex: 2, 3" />
         </p>
 
         <p>
-          <strong>Perícias</strong>
-          <input type="number" placeholder="Ex: 2, 3" />
+          <strong>Perícia</strong>
+          <input onChange={handleChange} type="number" name="skill" placeholder="Ex: 2, 3" />
         </p>
         <p>
           <strong>Equipamento</strong>
-          <input type="number" placeholder="Ex: 2, 3" />
+          <input onChange={handleChange} type="number" name="gear" placeholder="Ex: 2, 3" />
         </p>
       </header>
       
       <p className="paragraph-container">
-        <button type="button">Rolar dados!</button>
+        <button type="button" onClick={handleClick}>Rolar dados!</button>
         <button type="button">Push!</button>
       </p>
 
       <div className="dices-container">
         <h2>Atributo</h2>
         <div className="dice-container">
-          {[1,3,4,3,3,2,6].map(number => <DiceComponent color="white" value={number} /> )}
+          {state.attributesDices.map((number, index) => <DiceComponent key={index} color="white" value={number} /> )}
         </div>
         <h2>Perícia</h2>
         <div className="dice-container">
-          {[1,3,4,3,3,2,6].map(number => <DiceComponent color="red" value={number} /> )}
+          {state.skillDices.map((number, index) => <DiceComponent key={index} color="red" value={number} /> )}
         </div>
         <h2>Equipamento</h2>
         <div className="dice-container">
-          {[1,3,4,3,3,2,6].map(number => <DiceComponent color="black" value={number} /> )}
+          {state.gearDices.map((number, index) => <DiceComponent key={index} color="black" value={number} /> )}
         </div>
       </div>
     </PageDefault>
